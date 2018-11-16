423fc11ab0330c92b831e0a3d476b1c99a8be65a
SterlingPerry <sterling.perry@georgiacourts.gov>
2018-11-15 21:29:26 -0500

    Adds socket.io to server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const todoController = require('./controllers/todoController');
-
 const app = express();
+
+const io = require('socket.io')(server);
+const mongoose = require('mongoose');
+const PORT  = process.env.PORT || 4000;
 
 //set up template engine
 
 app.set('view engine', 'ejs');
 
 //static files
-
+app.use(express.json());
 app.use(express.static('./public'));
 
 //fire controllers
